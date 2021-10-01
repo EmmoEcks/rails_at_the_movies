b637d6a78029ab2b8872ad08135467d08400db1d
Ariel Ardiles <p.a.ardiles@gmail.com>
2021-10-01 12:59:18 -0500

    created seeds.rb to populate the database

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :production_company
-  validates :title, year, :duration, :description, :average_vote, presence: true
+  validates :title, :year, :duration, :description, :avg_vote, presence: true
   validates :title, uniqueness:true
-  validates :year, :duration, numericality: true { only_integer: true }
+  validates :year, :duration, numericality: { only_integer: true }
   validates :average_vote, numericality: true
 end
